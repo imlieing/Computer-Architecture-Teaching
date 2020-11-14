180731111206220d37602ce93732c17d67ff15f8
aw868 <aw868@drexel.edu>
2020-11-14 13:25:53 -0500

    finished project 3

--- C621/Cache_Policy/run_sim.sh
+++ C621/Cache_Policy/run_sim.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./Main 531.deepsjeng_r_llc.mem_trace >> lru_deep.txt;
+./Main 531.deepsjeng_r_llc.mem_trace >> LFU_deep.txt;
 echo "done deep"
-./Main 541.leela_r_llc.mem_trace >> lru_leela.txt;
+./Main 541.leela_r_llc.mem_trace >> LFU_leela.txt;
 echo "done leela"
-./Main 548.exchange2_r_llc.mem_trace >> lru_exchange.txt;
+./Main 548.exchange2_r_llc.mem_trace >> LFU_exchange.txt;
 echo "done exchange"
