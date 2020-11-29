ff8c0ba612268f03947517d8d1483ba388c26059
aw868 <aw868@drexel.edu>
2020-11-29 12:12:43 -0500

    added prefetcher trace to git ignore, run_sim

--- C621/Cache_Policy/run_sim.sh
+++ C621/Cache_Policy/run_sim.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-./Main 531.deepsjeng_r_llc.mem_trace >> arc_deep.txt;
+./Main blender.l1.trace >> nextLine_blender.txt;
 echo "done deep"
-./Main 541.leela_r_llc.mem_trace >> arc_leela.txt;
+./Main cactuBSSN.l1.trace >> nextLine_cactu.txt;
 echo "done leela"
-./Main 548.exchange2_r_llc.mem_trace >> arc_exchange.txt;
+./Main omnetpp.l1.trace >> nextLine_omnetpp.txt;
 echo "done exchange"
-=======
-./Main 531.deepsjeng_r_llc.mem_trace >> LFU_deep.txt;
-echo "done deep"
-./Main 541.leela_r_llc.mem_trace >> LFU_leela.txt;
-echo "done leela"
-./Main 548.exchange2_r_llc.mem_trace >> LFU_exchange.txt;
-echo "done exchange"
->>>>>>> 180731111206220d37602ce93732c17d67ff15f8
