f76e7c90a0351d8a9a0c717c694726a98eaa9183
aw868 <aw868@drexel.edu>
2020-11-29 12:17:11 -0500

    reorganized data files, added nextline and stride txt files

--- C621/Cache_Policy/run_sim.sh
+++ C621/Cache_Policy/run_sim.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ./Main blender.l1.trace >> nextLine_blender.txt;
-echo "done deep"
+echo "done blender"
 ./Main cactuBSSN.l1.trace >> nextLine_cactu.txt;
-echo "done leela"
+echo "done cactu"
 ./Main omnetpp.l1.trace >> nextLine_omnetpp.txt;
-echo "done exchange"
+echo "done omnetpp"
